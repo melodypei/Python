2eb24433f8a327858a278e04dd1e3588553a16cb
melodypei <33191863+melodypei@users.noreply.github.com>
2018-11-26 12:39:07 +0800

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -3,9 +3,11 @@
 
 import time
 import datetime
+import functools
 
 
 def timeIt(func):
+    @functools.wraps(func)
     def warp(arg):
         start = datetime.datetime.now()
         func(arg)
@@ -15,11 +17,15 @@ def timeIt(func):
     return warp
 
 @timeIt
-def func(arg):
+def func2(arg):
+    '''this is in func2'''
     time.sleep(arg)
 
 
-func(3)
+func2(3)
+print (func2.__name__)
+print (func2.__doc__)
+print (func2.__module__)
 
     
     
