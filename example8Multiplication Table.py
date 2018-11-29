bd3069edbdf0a55f4ff50122614e43d2e7a4fea7
melodypei <33191863+melodypei@users.noreply.github.com>
2018-11-29 21:37:10 +0800

    Add files via upload

---
+++ example8Multiplication Table.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+
+for  i in range (1,10):
+    print ("\n")
+    for j in range(1,i+1):
+        print ("%d*%d=%d " % (i,j,i*j),end = '')
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+                
+    
+    
