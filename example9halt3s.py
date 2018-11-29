48b320a1d66dee37f5bcce69d59db8ec5dfc2679
melodypei <33191863+melodypei@users.noreply.github.com>
2018-11-29 21:45:46 +0800

    Add files via upload

---
+++ example9halt3s.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+import time
+
+mydic = {1:"a",2:"b"}
+for k ,v in dict.items(mydic):
+    print (k ,v)
+    time.sleep(3)
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+                
+    
+    
