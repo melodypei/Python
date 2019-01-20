c0bf41d613f25d2064cb3df2a9bf18f2147efd3d
melodypei <33191863+melodypei@users.noreply.github.com>
2019-01-20 15:29:36 +0800

    Update and rename example01 to example01.py

---
+++ example01.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+
+'''
+有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+'''
+
+a = 0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if (i!=j)and (j!=k)and(i!=k):
+                print (i,j,k)
+                a += 1
+print(a)
+
+                
+    
+    
